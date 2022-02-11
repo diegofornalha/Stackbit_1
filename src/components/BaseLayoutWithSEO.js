16b3fc67cb1144cede0eff77c74a4b3d228fc9fa
Stackbit Code Editor <projects@stackbit.com>
2022-02-11 13:39:30 +0000

    BaseLayoutWithSEO.js: changed

--- src/components/BaseLayoutWithSEO.js
+++ src/components/BaseLayoutWithSEO.js
@@ -12,7 +12,6 @@ export default function BaseLayoutWithSEO(props) {
     const Header = getComponent('Header');
     const Footer = getComponent('Footer');
     const pageTitle = _.get(page, 'title');
-    const config = _.get(this.props, 'config');
     const configTitle = _.get(config, 'title');
     const seo = _.get(page, 'seo');
     const seoTitle = _.get(seo, 'title');
