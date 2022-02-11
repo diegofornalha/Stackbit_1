c1c106f3a4f22cf76330c702af1e662636f4a4a5
Stackbit Code Editor <projects@stackbit.com>
2022-02-11 12:59:49 +0000

    layout.js: changed

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import _ from 'lodash';
+import {} from '@stackbit/components'
 
 import { withPrefix } from '../utils';
 import Header from './Header';
