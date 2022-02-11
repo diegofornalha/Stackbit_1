2ce54eaefa3ee482e3e3526cd394542f3f47d603
Stackbit Code Editor <projects@stackbit.com>
2022-02-11 13:31:45 +0000

    register-components.js: changed

--- src/components/register-components.js
+++ src/components/register-components.js
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 import { registerComponents } from '@stackbit/components';
 import { componentsMap } from '@stackbit/components/dist/components-map';
-import PageLayout from './layout'
+import PageLayout from './CustomPageLayout'
 
 registerComponents({
     // Register all Stackbit components
