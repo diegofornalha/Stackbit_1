65c498218e4bc76329e10286cc81884302a965d0
Stackbit Code Editor <projects@stackbit.com>
2022-02-24 10:05:18 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,7 +8,8 @@ export default function MyApp({ Component, pageProps }) {
         <>
             <Head>
                 <link rel="icon" href="/favicon.png" />
-                <Script id=></Script>
+                <Script id=
+                ></Script>
 <script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
