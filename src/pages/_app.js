a796ff4441c6ee013e38bde8b6c54032d0659702
Stackbit Code Editor <projects@stackbit.com>
2022-02-24 10:06:37 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,9 +9,9 @@ export default function MyApp({ Component, pageProps }) {
             <Head>
                 <link rel="icon" href="/favicon.png" />
                 <Script id="gtm-script" strategy="lazyOnload">
-                    
+
                 </Script>
-<script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
