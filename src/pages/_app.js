14884845e0c2282d6744504fb41573774cc79892
Stackbit Code Editor <projects@stackbit.com>
2022-02-24 10:10:02 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,6 +8,7 @@ export default function MyApp({ Component, pageProps }) {
         <>
             <Head>
                 <link rel="icon" href="/favicon.png" />
+            </Head>
                 <Script id="gtm-script" strategy="lazyOnload">
                     {`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
                                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -15,7 +16,6 @@ export default function MyApp({ Component, pageProps }) {
                                     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                     })(window,document,'script','dataLayer','GTM-5SMVSWG');`}
                 </Script>
-            </Head>
             <Component {...pageProps} />
         </>
     );
