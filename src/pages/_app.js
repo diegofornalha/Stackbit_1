c613ee4de57f1fcf808fa1ec2ff1bf84581122eb
Stackbit Code Editor <projects@stackbit.com>
2022-02-24 10:08:21 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,12 +10,12 @@ export default function MyApp({ Component, pageProps }) {
                 <link rel="icon" href="/favicon.png" />
                 <Script id="gtm-script" strategy="lazyOnload">
 
+                    {`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+                                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-5SMVSWG');`}
                 </Script>
-{`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5SMVSWG');`}
             </Head>
             <Component {...pageProps} />
         </>
