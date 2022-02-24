24fa05e37435322012f4c6f0d92d30a5b602f457
Stackbit Code Editor <projects@stackbit.com>
2022-02-24 10:02:25 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,12 +8,12 @@ export default function MyApp({ Component, pageProps }) {
             <Head>
                 <link rel="icon" href="/favicon.png" />
                 <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+<script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5SMVSWG');</script>
-<!-- End Google Tag Manager -->
+                <!-- End Google Tag Manager -->
             </Head>
             <Component {...pageProps} />
         </>
