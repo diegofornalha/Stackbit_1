a30ff92fdd51d34de5ebd49cee495498361c3487
Stackbit Code Editor <projects@stackbit.com>
2022-02-24 10:04:47 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import '../components/register-components';
 import '../css/main.css';
 import Head from 'next/head';
-import Head from 'next/head';
+import Script from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
     return (
