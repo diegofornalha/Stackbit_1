5b98bae6907d98d9d44c8a9ef487ff46b8ece90c
Stackbit Code Editor <projects@stackbit.com>
2022-03-03 20:30:09 +0000

    PricingSection.js: changed

--- src/components/PricingSection.js
+++ src/components/PricingSection.js
@@ -121,11 +121,14 @@ class Pricing extends React.Component {
         return (
             <div className='pricing'>
                 <h2 className='text-center'>Áraink</h2>
+                <div>
+
                 <label for="toggle-example" class="flex items-center cursor-pointer relative mb-4">
                     <input type="checkbox" id="toggle-example" class="sr-only" />
                     <div class="toggle-bg bg-gray-200 border-2 border-gray-200 h-6 w-11 rounded-full"></div>
                     <span class="ml-3 text-gray-900 text-sm font-medium">Toggle me</span>
                 </label>
+                </div>
 
                 <div className='pricing-slider center-content'>
                     <label className='form-slider'>
