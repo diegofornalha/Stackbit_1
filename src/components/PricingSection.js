149a0b822a4de929dbd943d3c92a357cf2fff981
Stackbit Code Editor <projects@stackbit.com>
2022-03-03 20:32:01 +0000

    PricingSection.js: changed

--- src/components/PricingSection.js
+++ src/components/PricingSection.js
@@ -124,7 +124,7 @@ class Pricing extends React.Component {
                 <div className='flex justify-center items-center'>
 
                     <span class="mr-3 text-gray-900 text-sm font-medium">Havi</span>
-                    <label for="toggle-example" class="flex items-center cursor-pointer relative>
+                    <label for="toggle-example" class="flex items-center cursor-pointer relative">
                         <input type="checkbox" id="toggle-example" class="sr-only" />
                         <div class="toggle-bg bg-gray-200 border-2 border-gray-200 h-6 w-11 rounded-full"></div>
                         <span class="ml-3 text-gray-900 text-sm font-medium">Éves</span>
