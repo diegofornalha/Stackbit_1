ad9fc892910fbf6044935e6f5371b97bd5e0af8b
Stackbit Code Editor <projects@stackbit.com>
2022-03-03 20:10:55 +0000

    PricingSection.js: changed

--- src/components/PricingSection.js
+++ src/components/PricingSection.js
@@ -185,7 +185,7 @@ class Pricing extends React.Component {
                                 </div>
                                 <div className='pricing-item-features'>
                                     <ul className='pricing-item-features-list'>
-                                        <li className='is-checked'>Excepteur sint occaecat</li>
+                                        <li className='is-checked'>Minden ami az ingyenesben</li>
                                         <li className='is-checked'>Excepteur sint occaecat</li>
                                         <li className='is-checked'>Excepteur sint occaecat</li>
                                         <li className='is-checked'>Excepteur sint occaecat</li>
