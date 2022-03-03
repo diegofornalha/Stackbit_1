f712fc6406fcca5efe8c9530bbaccdb077b1705e
Stackbit Code Editor <projects@stackbit.com>
2022-03-03 19:14:53 +0000

    register-components.js: changed

--- src/components/register-components.js
+++ src/components/register-components.js
@@ -3,14 +3,13 @@ import { registerComponents } from '@stackbit/components';
 import { componentsMap } from '@stackbit/components/dist/components-map';
 import PageLayout from './CustomPageLayout'
 import PostLayout from './CustomPostLayout'
-import PricingSection from './PricingSection'
 
 registerComponents({
     // Register all Stackbit components
     ...componentsMap,
     PageLayout,
     PostLayout,
-    PricingSection: dynamic(() => import())
+    PricingSection: dynamic(() => import('./PricingSection'))
 
     // Override any static or dynamic component,
     // or register your own dynamic component.
