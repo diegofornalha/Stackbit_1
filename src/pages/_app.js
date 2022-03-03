1860e4b2aa489f5dabd135a5d1f2ae820bebf37d
Stackbit Code Editor <projects@stackbit.com>
2022-03-03 14:21:17 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -16,7 +16,6 @@ export default function MyApp({ Component, pageProps }) {
                                     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                     })(window,document,'script','dataLayer','GTM-5SMVSWG');`}
                 </Script>
-
             <Component {...pageProps} />
         </>
     );
