b5fb95adbe9cc9f1ee2803f44f26a22fbe0281a4
Stackbit Code Editor <projects@stackbit.com>
2022-03-04 15:38:31 +0000

    PricingSection.js: changed

--- src/components/PricingSection.js
+++ src/components/PricingSection.js
@@ -197,7 +197,7 @@ class Pricing extends React.Component {
                                         {this.getPricingData(this.state.isYearly ? this.state.priceOutput.plan2 : this.state.priceOutput.plan1, 2)}
                                     </div>
                                 </div>
-                            <em className='text-center'>ingyenes</h6>
+                            <em className='text-center'>ingyenes</em>
                                 <div className='pricing-item-features'>
                                     <ul className='pricing-item-features-list'>
                                         <li className='is-checked'>Minden ami az ingyenesben</li>
