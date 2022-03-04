de170828cd2e9ed5a4e7e816ea4ecad9a7a26f08
Stackbit Code Editor <projects@stackbit.com>
2022-03-04 12:07:40 +0000

    PricingSection.js: changed

--- src/components/PricingSection.js
+++ src/components/PricingSection.js
@@ -165,7 +165,7 @@ class Pricing extends React.Component {
                                         <li className='is-checked'>Több szálláshely és szobák egy helyen</li>
                                         <li className='is-checked'>Naprakész naptár</li>
                                         <li className='is-checked'>Testreszabható automatikus emailek</li>
-                                        <li className='is-checked'>Testreszabható automatikus emailek</li>
+                                        <li className='is-checked'>Képek feltöltése</li>
                                         <li className='is-checked'>
                                             Mobilbarát és modern kialakítás
                                         </li>
