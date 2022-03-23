a17572ecc1810ef9a82a5b4e4652e89504d516c5
Stackbit Code Editor <projects@stackbit.com>
2022-03-23 10:16:08 +0000

    PricingSection.js: changed

--- src/components/PricingSection.js
+++ src/components/PricingSection.js
@@ -146,25 +146,6 @@ class Pricing extends React.Component {
                     </div>
                 </div>
 
-                <div className='pricing-items'>
-                    <div className='pricing-item'>
-                        <div className='pricing-item-inner'>
-                            <div className='pricing-item-content'>
-                                <div className='pricing-item-header center-content'>
-                                    <div className='pricing-item-title'>Alap csomag</div>
-                                    <div className='pricing-item-price'>
-                                        <span className='pricing-item-price-amount'>
-                                            {/* {this.getPricingData(this.state.priceOutput.plan1, 1)} */}
-                                            Ingyenes
-                                        </span>
-                                    </div>
-                                </div>
-                                <div className='pricing-item-features'>
-                                    <ul className='pricing-item-features-list'>
-                                        <li className='is-checked'>Több szálláshely és szoba egy helyen</li>
-                                        <li className='is-checked'>Naprakész naptár</li>
-                                        <li className='is-checked'>Testreszabható automatikus emailek</li>
-                                        <li className='is-checked'>Képek feltöltése</li>
                     <div className='pricing-item'>
                         <div className='pricing-item-inner'>
                             <div className='pricing-item-content'>
@@ -206,6 +187,25 @@ class Pricing extends React.Component {
                             </div>
                         </div>
                     </div>
+                <div className='pricing-items'>
+                    <div className='pricing-item'>
+                        <div className='pricing-item-inner'>
+                            <div className='pricing-item-content'>
+                                <div className='pricing-item-header center-content'>
+                                    <div className='pricing-item-title'>Alap csomag</div>
+                                    <div className='pricing-item-price'>
+                                        <span className='pricing-item-price-amount'>
+                                            {/* {this.getPricingData(this.state.priceOutput.plan1, 1)} */}
+                                            Ingyenes
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className='pricing-item-features'>
+                                    <ul className='pricing-item-features-list'>
+                                        <li className='is-checked'>Több szálláshely és szoba egy helyen</li>
+                                        <li className='is-checked'>Naprakész naptár</li>
+                                        <li className='is-checked'>Testreszabható automatikus emailek</li>
+                                        <li className='is-checked'>Képek feltöltése</li>
                                         <li className='is-checked'>
                                             Mobilbarát és modern kialakítás
                                         </li>
