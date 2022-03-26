63ce89eeb999f7a0c160268dcd94f17f897e0020
Stackbit Code Editor <projects@stackbit.com>
2022-03-26 10:48:59 +0000

    accordion.js: changed

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -19,7 +19,7 @@ for (i=0; i<accordion.length; i++) {
 <div class="accordion-body">
   <div class="accordion">
     <h1>Frequently Asked Questions</h1>
-    <hr>
+
     <div class="container">
       <div class="label">What is HTML</div>
       <div class="content">Hypertext Markup Language (HTML) is a computer language that makes up most web pages and online applications. A hypertext is a text that is used to reference other pieces of text, while a markup language is a series of markings that tells web servers the style and structure of a document. HTML is very simple to learn and use.</div>
