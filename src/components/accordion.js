c577e6be5ee1ac74302be6255cbd55437e8bd699
Stackbit Code Editor <projects@stackbit.com>
2022-03-26 10:27:57 +0000

    accordion.js: changed

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -9,31 +9,30 @@ export default function accordion(props) {
 import React from 'react'
 
 class Pricing extends React.Component {
+
+    var acc = document.getElementsByClassName("accordion");
+    var i;
     
-}
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+    
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
     }
-  });
+    
+    return(
+        <button class="accordion">Section 1</button>
+        <div class="panel">
+            <p>Lorem ipsum...</p>
+        </div>
+    )
 }
-
-return(
-    <button class="accordion">Section 1</button>
-    <div class="panel">
-        <p>Lorem ipsum...</p>
-    </div>
-)
