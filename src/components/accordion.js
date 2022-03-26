72343b4d8177bf06811f8431d4e1c71bd176c7cd
Stackbit Code Editor <projects@stackbit.com>
2022-03-26 10:46:31 +0000

    accordion.js: changed

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -10,7 +10,7 @@ import React from 'react'
 
 
 
-const accordion = document.getElementsByClassName('container');
+
 
 for (i=0; i<accordion.length; i++) {
   accordion[i].addEventListener('click', function () {
