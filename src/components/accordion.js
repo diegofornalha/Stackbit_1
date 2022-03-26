eece43602a99165a9b55ce86902e3e435e0dbbd8
Stackbit Code Editor <projects@stackbit.com>
2022-03-26 10:27:29 +0000

    accordion.js: changed

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -8,7 +8,7 @@ export default function accordion(props) {
 
 import React from 'react'
 
-class Pricing extends React.Component {
+class Pricing extends React.Component 
 
 var acc = document.getElementsByClassName("accordion");
 var i;
