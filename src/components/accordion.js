61ae9ab5664c938382b675bd6e7ef6a3a3c192e6
Stackbit Code Editor <projects@stackbit.com>
2022-03-26 10:52:42 +0000

    accordion.js: changed

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -12,7 +12,7 @@ import React from 'react'
 
 
 
-for (i=0; i<accordion.length; i++) {
+for (let i=0; i<accordion.length; i++) {
   accordion[i].addEventListener('click', function () {
     this.classList.toggle('active')
   return (
