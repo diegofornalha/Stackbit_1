91496ce3aaad172386fc7a0eacb7558589fa58db
Stackbit Code Editor <projects@stackbit.com>
2022-05-31 09:40:37 +0000

    PricingSection.js: changed

--- src/components/PricingSection.js
+++ src/components/PricingSection.js
@@ -129,7 +129,7 @@ class Pricing extends React.Component {
                         <div class="toggle-bg bg-gray-200 border-2 border-gray-200 h-6 w-11 rounded-full"></div>
                         <span class="ml-3 text-gray-900 text-sm font-medium">Éves</span>
                     </label>
-                </div>
+                </div>//
 
                 <div className='pricing-slider center-content'>
                     <label className='form-slider'>
