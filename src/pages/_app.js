6a2d0e3f374566fe872c0889fef56bfcb11c3ada
Stackbit Code Editor <projects@stackbit.com>
2022-05-31 12:00:07 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,24 +10,24 @@ export default function MyApp({ Component, pageProps }) {
                 <link rel="icon" href="/favicon.png" />
             </Head>
                 <Script>
-                {/* // Selecting the iframe element */}
-                var frame = document.getElementById("Iframe");
+                    {/* // Selecting the iframe element */}
+                    var frame = document.getElementById("Iframe");
 
-                
-                {/* // Adjusting the iframe height onload event */}
-                frame.onload = function()
-                // function execute while load the iframe
-                {
-                // set the height of the iframe as 
-                // the height of the iframe content
-                frame.style.height = frame.contentWindow.document.body.scrollHeight + '1px';
-                
-        
-                // set the width of the iframe as the 
-                // width of the iframe content
-                frame.style.width = frame.contentWindow.document.body.scrollWidth+'1px';
                     
-                }
+                    {/* // Adjusting the iframe height onload event */}
+                    frame.onload = function()
+                    // function execute while load the iframe
+                    {
+                    // set the height of the iframe as 
+                    // the height of the iframe content
+                    frame.style.height = frame.contentWindow.document.body.scrollHeight + '1px';
+                    
+            
+                    // set the width of the iframe as the 
+                    // width of the iframe content
+                    frame.style.width = frame.contentWindow.document.body.scrollWidth+'1px';
+                        
+                    }
                 </Script>
                 <Script id="gtm-script" strategy="lazyOnload">
                     {`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
