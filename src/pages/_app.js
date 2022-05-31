1ef97e709bc494d0bfb75fec39dfc8e989c96cb6
Stackbit Code Editor <projects@stackbit.com>
2022-05-31 11:46:17 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -23,9 +23,8 @@ export default function MyApp({ Component, pageProps }) {
                         resizeIFrameToFitContent( iFrame );
                         }
                     } );
+                    
 
-                </Script>
-                
                 <Script id="gtm-script" strategy="lazyOnload">
                     {`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
                                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
