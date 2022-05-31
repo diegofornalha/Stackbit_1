99f9e4662621ff802b7ae300fc89a619d50adbeb
Stackbit Code Editor <projects@stackbit.com>
2022-05-31 11:43:25 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,23 +11,23 @@ export default function MyApp({ Component, pageProps }) {
             </Head>
                 <Script type="application/javascript">
 
-function resizeIFrameToFitContent( iFrame ) {
+                    function resizeIFrameToFitContent( iFrame ) {
 
-    iFrame.width  = iFrame.contentWindow.document.body.scrollWidth;
-    iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
-}
+                        iFrame.width  = iFrame.contentWindow.document.body.scrollWidth;
+                        iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
+                    }
 
-window.addEventListener('DOMContentLoaded', function(e) {
+                    window.addEventListener('DOMContentLoaded', function(e) {
 
-    var iFrame = document.getElementById( 'iFrame1' );
-    resizeIFrameToFitContent( iFrame );
+                        var iFrame = document.getElementById( 'iFrame1' );
+                        resizeIFrameToFitContent( iFrame );
 
-    // or, to resize all iframes:
-    var iframes = document.querySelectorAll("iframe");
-    for( var i = 0; i < iframes.length; i++) {
-        resizeIFrameToFitContent( iframes[i] );
-    }
-} );
+                        // or, to resize all iframes:
+                        var iframes = document.querySelectorAll("iframe");
+                        for( var i = 0; i < iframes.length; i++) {
+                            resizeIFrameToFitContent( iframes[i] );
+                        }
+                    } );
 
                 </Script>
                 <Script id="gtm-script" strategy="lazyOnload">
