b14c980f1812ccec814d8d412e6cf5fba5e8b14f
Stackbit Code Editor <projects@stackbit.com>
2022-05-31 12:34:37 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,7 +11,7 @@ export default function MyApp({ Component, pageProps }) {
             </Head>
                 <Script>
                     function resizeIframe (Object) {
-                        Object.style.height
+                        Object.style.height 
                     }
                 </Script>
                 <Script id="gtm-script" strategy="lazyOnload">
