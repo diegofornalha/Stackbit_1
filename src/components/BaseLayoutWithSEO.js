435fea447f517616971dec7a1c6b1eb5817c1f1e
Stackbit Code Editor <projects@stackbit.com>
2022-06-24 18:53:39 +0000

    BaseLayoutWithSEO.js: changed

--- src/components/BaseLayoutWithSEO.js
+++ src/components/BaseLayoutWithSEO.js
@@ -50,8 +50,7 @@ export default function BaseLayoutWithSEO(props) {
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
                     <Script>
-                        {`
-                          !function(f,b,e,v,n,t,s)
+                        {`!function(f,b,e,v,n,t,s)
   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
   n.callMethod.apply(n,arguments):n.queue.push(arguments)};
   if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
