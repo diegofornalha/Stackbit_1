2acdaeabcb9279d6f0991c5258bdca2f52a51b5c
Stackbit Code Editor <projects@stackbit.com>
2022-06-24 18:52:29 +0000

    BaseLayoutWithSEO.js: changed

--- src/components/BaseLayoutWithSEO.js
+++ src/components/BaseLayoutWithSEO.js
@@ -50,7 +50,18 @@ export default function BaseLayoutWithSEO(props) {
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
                     <Script>
-                        {``}
+                        {`
+                          !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '{your-pixel-id-goes-here}');
+  fbq('track', 'PageView');
+                        `}
                     </Script>
                 </Head>
                 {site.header && <Header {...site.header} annotationPrefix={siteMeta.id} />}
